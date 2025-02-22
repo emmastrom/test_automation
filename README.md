35a3926fa5bbb57c188fb4e6b7d1d499412819fc
Emma Lundström <1emma.lundstrom@gmail.com>
2025-02-22 18:17:13 +0200

    amazon tests + updated readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# test_automation
+# Test automation using playwright framework
+
+The code runs the following automated tests for amazon.com:
+*  Check the correct website is open
+*  Can search for "Nikon"
+*  Can sort "Nikon" search results from highest to lowest price
+*  Can select the second product where the product title containd D6 -> under review
+
+After cloning the repo run
+`npm install`
+to install dependencies.
+
+To run amazon related tests run the following command:
+`npx playwright test amazon.spec.ts`
+
+If you want to watch the browser as the tests are run, you can run the command:
+`npx playwright test amazon.spec.ts --headed`
+
+The tests are run parallel. So if you use this command the browser windows for all tests open at the same time.
+
---
+++ tests/amazon.spec.ts
@@ -0,0 +1,76 @@
+import { test, expect } from '@playwright/test';
+
+test.describe.configure({ mode: 'parallel' });
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('https://www.amazon.com/');
+    // While running tests locally with Chrome browser the page sometimes opened differently, this was fixed by reloading the page once
+    await page.reload();
+  });
+
+
+test.describe('in amazon.com', () => {
+    test('the website opens correctly', async ({ page }) => {
+        // Check the webpage has Amazon logo-link
+        await expect(page.getByRole('link', {name: 'Amazon'}).first()).toBeVisible();
+        });
+    
+    test.describe('"Nikon" search', () => {
+        test('gets results', async ({ page }) => {
+            // Input "Nikon" into searchbox
+            await page.getByRole('searchbox', {name: 'Search Amazon'}).fill('Nikon');
+    
+            // Click search
+            await page.getByRole('button', {name: 'GO'}).first().click();
+    
+            // Check the search yields results
+            await expect(page.getByRole('heading', {name: 'Results', exact: true})).toBeVisible();
+        });
+    
+        test('sorted from highest to lowest price works', async ({ page }) => {
+            // Search "Nikon"
+            await page.getByRole('searchbox', {name: 'Search Amazon'}).fill('Nikon');
+            await page.getByRole('button', {name: 'GO'}).first().click();
+            await expect(page.getByRole('heading', {name: 'Results', exact: true})).toBeVisible();
+    
+            // Create locator for sortby dropdown button
+            const sortby = page.locator('span#a-autoid-0-announce');
+    
+            sortby.click();
+    
+            // Create locator for Price: High to Low in dropdown
+            const highToLow = page.locator('a#s-result-sort-select_2');
+    
+            highToLow.click();
+    
+            // Create locator for text in Sort by dropdown container
+            const sorted = page.locator('span.a-dropdown-prompt')
+    
+            // Check search results are sorted from highest to lowest price
+            await expect(sorted).toContainText('Price: High to Low');
+        })
+
+        test('second product once clicked for more details has title containing D6', async({ page }) => {
+            // Search "Nikon"
+            await page.getByRole('searchbox', {name: 'Search Amazon'}).fill('Nikon');
+            await page.getByRole('button', {name: 'GO'}).first().click();
+            await expect(page.getByRole('heading', {name: 'Results', exact: true})).toBeVisible();
+
+            // Sort search results from highest to lowest price
+            const sortby = page.locator('span#a-autoid-0-announce');
+            sortby.click({delay: 1000});
+
+            const highToLow = page.locator('a#s-result-sort-select_2');
+            highToLow.click({delay: 1000});
+    
+            const sorted = page.locator('span.a-dropdown-prompt')
+            await expect(sorted).toContainText('Price: High to Low');
+
+            // Select second product
+            await page.getByRole('link', {name: 'See options'}).nth(1).click();
+
+            // Check product title for "Nikon D6"
+            await expect(page.getByRole('heading', {name: 'Nikon D6'})).toBeVisible();
+        })
+    })
+})
